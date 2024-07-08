3037b4eec0aef0d29cc5fb9fc6680fdb8bc5bc1b
darkjaffs <hurairajaffs2003@gmail.com>
2024-07-08 23:48:53 +0500

    Complete the Calculator App

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
-
-let digitOne = 0;
-let digitTwo = 0;
-let operator;
-let displayValue = 0;
+let digitOne = null;
+let digitTwo = null;
+let operator = null;
+let displayValue = null;
+let keyPressed = null;
+let operatorPressed = 0;
 
 function add(first, second) {
+    first = parseInt(first);
+    second = parseInt(second);
     return first + second;
 }
 
@@ -34,10 +37,85 @@ function operate(operation, first, second) {
         return subtract(first, second);
     else if (operation === "*")
         return multiply(first, second);
-    else
+    else {
         return divide(first, second);
+    }
 }
 
 
 
+function display(input) {
 
+    const text = document.getElementById("screen");
+
+    if (input === undefined)
+        text.textContent = 0;
+    else
+        text.textContent = input;
+
+
+}
+
+function clear() {
+
+    const text = document.querySelector('#screen')
+    operator = null;
+    digitOne = null;
+    digitTwo = null;
+    operatorPressed = 0;
+    text.textContent = 0;
+}
+
+
+const main = document.querySelectorAll(".buttons")
+
+main.forEach(button => {
+    button.addEventListener("click", (e) => {
+
+        keyPressed = e.target.textContent;
+
+        if (keyPressed === 'AC') {
+            clear();
+        }
+        else if (!isNaN(Number(keyPressed))) {
+            if (digitTwo === null && operator === null) {
+                if (digitOne === null) {
+                    digitOne = keyPressed;
+                    display(digitOne);
+                }
+                else {
+                    digitOne = digitOne + keyPressed;
+                    display(digitOne);
+                }
+            }
+            else {
+                if (digitTwo === null) {
+                    digitTwo = keyPressed;
+                    display(digitTwo);
+                }
+                else {
+                    digitTwo = digitTwo + keyPressed;
+                    display(digitTwo);
+                }
+            }
+
+        }
+        else if (isNaN(Number(keyPressed)) && keyPressed != "RM" && keyPressed != "=") {
+            operator = keyPressed;
+            if (operatorPressed != 1) {
+                operatorPressed = 1;
+            }
+            else {
+                displayValue = operate(operator, digitOne, digitTwo);
+                display(displayValue);
+                digitOne = displayValue;
+                digitTwo = null;
+            }
+        }
+        else if (keyPressed === '=' && operator != null && digitOne != null && digitTwo != null) {
+            displayValue = operate(operator, digitOne, digitTwo);
+            clear();
+            display(displayValue);
+        }
+    })
+});
